753dcf1ef3dfc9b08e480ee3bc9496ae3f27797c
jeangho <jeangho293@naver.com>
2021-11-30 22:03:06 +0900

    [완전탐색] 모의고사

---
+++ [Level 1]/[완전탐색] 모의고사/모의고사.py
@@ -0,0 +1,22 @@
+# 완전 탐색
+def solution(answers):
+    # 학생들의 규칙적인 답안을 리스트로 정리 및 필요한 길이만큼 연장
+    student_1 = [1, 2, 3, 4, 5]
+    student_2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    student_3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+
+    student_1 = student_1 * (len(answers) // len(student_1)) + student_1[:len(answers) % len(student_1)]
+    student_2 = student_2 * (len(answers) // len(student_2)) + student_2[:len(answers) % len(student_2)]
+    student_3 = student_3 * (len(answers) // len(student_3)) + student_3[:len(answers) % len(student_3)]
+
+    # 3명이 맞은 갯수를 저장한다.
+    rank = [0] * 3
+    for i, v in enumerate(answers):
+        if student_1[i] == v:
+            rank[0] += 1
+        if student_2[i] == v:
+            rank[1] += 1
+        if student_3[i] == v:
+            rank[2] += 1
+
+    return [i + 1 for i in range(3) if rank[i] == max(rank)]
